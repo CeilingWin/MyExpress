11a376e972458ed46017fd4b6d9f8bd840f0ea88
Thắng Trần <thangftn99@gmail.com>
2021-05-08 16:39:27 +0700

    implement route, middleware

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const MyExpress = require('./lib/my-express');
+const express = require('express');
+const route = require('./lib/route');
 
 let app = MyExpress.createServer();
 
-app.get("/abc",function(req,res){
-    console.log(req.url);
+app.use((req,res,next)=>{
+    console.log("wellcome!");
+    next();
 });
-
 app.listen(3000);
+
+let router = new MyExpress.Router();
+console.log(router.createRegex("/product/:productID/detail/:detailID"));
+console.log(router.createRegex("/product/detail"));
+console.log(router.createRegex("/"));
+
+// let trg = new RegExp('^(\/product\/)(?<productID>\\w+)','g');
+// let a = trg.exec("/product/abc779");   
+// for (attr in a.groups){
+//     console.log(attr,":", a.groups[attr]);
+// }
+
+
+
+
